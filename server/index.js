55108b6853af1b871660e1f19aa3b84d9ac44986
Mark Castle <mark@castle.sx>
2016-04-19 17:21:42 -0700

    Conform to JS Standard

--- server/index.js
+++ server/index.js
@@ -1,36 +1,32 @@
-var express = require('express');
-var db = require('./db.js');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var router = express.Router();
-var bcrypt = require('bcrypt-nodejs');
+var express = require('express')
+var db = require('./db.js')
+var cors = require('cors')
+var router = express.Router()
+var bcrypt = require('bcrypt-nodejs')
 
-var app = express();
-app.use(cors());
+var app = express()
+app.use(cors())
 
-//checks username and password
-router.post('/homepage', function(request, response){
-  var username = request.body.username;
-  var password = request.body.password;
+// checks username and password
+router.post('/homepage', function (request, response) {
+  var username = request.body.username
+  var password = request.body.password
 
+  db.query('SELECT * FROM Users WHERE `username` = ?;', [username], function (err, rows) {
+    console.log('This is our password in our db', rows[0].password)
 
-  db.query('SELECT * FROM Users WHERE `username` = ?;', [username], function(err, rows) {
-    console.log("This is our password in our db", rows[0].password)
-    var hash = bcrypt.hashSync(password);
-
-    console.log("This is the bcrypt pass true/false",bcrypt.compareSync( password ,rows[0].password ))
+    console.log('This is the bcrypt pass true/false', bcrypt.compareSync(password, rows[0].password))
 
     if (err) {
-      throw err;
+      throw err
     } else {
-      if(!bcrypt.compareSync( password ,rows[0].password )){
-        console.log("Incorrect password");
-      }else{
-        response.send('/dashboard');
+      if (!bcrypt.compareSync(password, rows[0].password)) {
+        console.log('Incorrect password')
+      } else {
+        response.send('/dashboard')
       }
     }
   })
+})
 
-});
-
-module.exports = router;
+module.exports = router
