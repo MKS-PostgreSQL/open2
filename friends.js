fa936f87b1f3a69ec548733bab8b62151393cf9d
Danny Thai <dthai92@yahoo.com>
2016-04-20 16:50:07 -0700

    Implement route for accepting or sending a friend request

---
+++ friends.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var router = express.Router()
+var db = require('../../db.js')
+var sendError = require('../helpers.js').sendError
+var sendSuccess = require('../helpers.js').sendSuccess
+var sendData = require('../helpers.js').sendData
+var postData = require('../helpers.js').postData
+var findUserId = require('../helpers.js').findUserId
+
+//send a friend request to a user or accept a friend request
+router.post('/request', function (req, res) {
+  var username = req.headers.username
+  var friend = req.body.friend
+  var usernameId
+  var friendId
+  findUserId(username).then(function (id) {
+    usernameId = id
+  }).then(function () {
+    findUserId(friend).then(function (id) {
+      friendId = id
+    }).then(function () {
+      var insert = 'INSERT INTO Friends SET `user_id` = ?, `friend_id` = ?;'
+      db.query(insert, [usernameId, friendId], postData(res))
+    })
+  })
+})
+
+module.exports = router
