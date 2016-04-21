0a0375c1f4c6999956adb8c81591fc4d4d7c02e3
Danny Thai <dthai92@yahoo.com>
2016-04-21 16:17:41 -0700

    Add friends route

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors())
 var index = require('./index')
 var signup = require('./signup')
 var dashboard = require('./dashboard')
+var friends = require('./friends')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,6 +27,7 @@ app.use(morgan('dev'))
 app.use('/index', index)
 app.use('/signup', signup)
 app.use('/dashboard', dashboard)
+app.use('/friends', friends)
 
 var port = process.env.PORT || 8080
 
