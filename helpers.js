51eae3da2f660659853f8bae03c28d2db0b50ffd
Danny Thai <dthai92@yahoo.com>
2016-04-21 16:18:26 -0700

    Remove unused variables and fix spacing

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 var db = require('./db.js')
 var dt = {}
 
-//find user id of a user
-dt.findUserId = findUser = function (username) {
+// find user id of a user
+dt.findUserId = function (username) {
   var userId
   var query = 'SELECT `id` FROM Users WHERE `username` = ?;'
-  return new Promise (function (resolve) {
+  return new Promise(function (resolve) {
     db.query(query, [username], function (err, rows) {
       if (err) {
         console.error(err)
@@ -13,32 +13,33 @@ dt.findUserId = findUser = function (username) {
         userId = rows[0].id
         resolve(userId)
       }
-   })
+    })
   })
 }
 
-//success callback for POST or GET methods
-dt.sendSuccess = sendSuccess = function (res, method, rows) {
-  console.log('sendSuccess --- ROWS: ', rows)
+// success callback for POST or GET methods
+dt.sendSuccess = function (res, method, rows) {
   if (method === 'post') {
-    console.log('sendSuccess --- POST')
     res.status(201).json({success: true})
   } else if (method === 'get') {
-    console.log('sendSuccess --- GET')
     res.json({success: true, data: rows})
   } else {
     console.error('HTTP METHOD NOT PROVIDED!')
   }
 }
 
-//send error for POST or GET methods
-dt.sendError = sendError = function (res, code, err) {
+var sendSuccess = dt.sendSuccess
+
+// send error for POST or GET methods
+dt.sendError = function (res, code, err) {
   console.error(err)
   res.status(code).json({success: false})
 }
 
-//callback to retrieve data for GET requests
-dt.sendData = sendData = function (res) {
+var sendError = dt.sendError
+
+// callback to retrieve data for GET requests
+dt.sendData = function (res) {
   return function (err, rows) {
     if (err) {
       sendError(res, 404, err)
@@ -48,8 +49,8 @@ dt.sendData = sendData = function (res) {
   }
 }
 
-//callback for POST request
-dt.postData = postData = function (res) {
+// callback for POST request
+dt.postData = function (res) {
   return function (err, rows) {
     if (err) {
       sendError(res, 500, err)
